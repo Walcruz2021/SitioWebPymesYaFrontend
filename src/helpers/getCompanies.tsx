e7128052478095ed9313b7ed148e8029b54d7be8
omar cruz <cruzherreria1@gmail.com>
2023-04-12 21:59:11 -0300

    copygithub

--- src/helpers/getCompanies.tsx
+++ src/helpers/getCompanies.tsx
@@ -1,16 +1,9 @@
-// const getProducts = async () => {
-//     const response = await fetch('https://fakestoreapi.com/products');
-//     const data = await response.json();
-//     return data;
-// }
 
-// export default getProducts;
-
-const getCompanies = async () => {
-    //const response = await fetch('https://backendtiendavirtual.onrender.com/api/listCompanies');
-    const response = await fetch('https://backendcompanywalter.up.railway.app/api/listCompanies');
-    const data = await response.json();
-    return data;
-}
-
-export default getCompanies;
+const getCompaniesVip=async ()=>{
+    //const response=await fetch('https://backendtiendavirtual.onrender.com/api/listCompaniesByLevel') 
+    const response = await fetch('https://backendcompanywalter.up.railway.app/api/listCompaniesByLevel');
+    const data=await response.json()
+    return data
+    }
+    
+    export default getCompaniesVip
--- src/hooks/useFetchComp.tsx
+++ src/hooks/useFetchComp.tsx
@@ -44,5 +44,3 @@ const useFetchComp = () => {
 }
 
 export default useFetchComp;
-
-
