c05770c0f711ed0038fbf025026dad967e521122
omar cruz <cruzherreria1@gmail.com>
2023-04-22 18:36:11 -0300

    changeRuteBackend

--- src/helpers/rutaBackend.tsx
+++ src/helpers/rutaBackend.tsx
@@ -1,4 +1,5 @@
-const rutaBackend="https://backendcompanywalter.up.railway.app"
+//const rutaBackend="https://backendcompanywalter.up.railway.app"
+const rutaBackend="https://backendtiendavirtual.onrender.com"
 //const rutaBackend="http://localhost:3002"
 
 export default rutaBackend
