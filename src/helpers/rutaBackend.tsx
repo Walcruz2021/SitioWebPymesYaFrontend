42bbb561e67e21dfb30fcdbea23d865e835e7704
walterCuz <walcruz1988.21@gmail.com>
2023-10-30 22:04:56 -0300

    new variables deploy backend

--- src/helpers/rutaBackend.tsx
+++ src/helpers/rutaBackend.tsx
@@ -1,5 +1,7 @@
 
-const rutaBackend="https://backendcompanywalter.up.railway.app"
+const rutaBackend="https://backendcompanywalter-dev-qbzs.3.us-1.fl0.io/"
+//const rutaBackend="https://backendcompanywalter.up.railway.app"
+
 
 
 export default rutaBackend
