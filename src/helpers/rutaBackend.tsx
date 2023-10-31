548482178ed06e5f8c35bc185efa645daa129a44
walterCuz <walcruz1988.21@gmail.com>
2023-10-30 22:13:16 -0300

    modificacion link backend

--- src/helpers/rutaBackend.tsx
+++ src/helpers/rutaBackend.tsx
@@ -1,5 +1,5 @@
 
-const rutaBackend="https://backendcompanywalter-dev-qbzs.3.us-1.fl0.io/"
+const rutaBackend="https://backendcompanywalter-dev-qbzs.3.us-1.fl0.io"
 //const rutaBackend="https://backendcompanywalter.up.railway.app"
 
 
