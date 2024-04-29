fda994e257941cc0f7c919cbcf9320b5f9142c75
walterCuz <walcruz1988.21@gmail.com>
2024-04-29 09:37:51 -0300

    error 404 redirect

--- src/pages/Home/Error404.jsx
+++ src/pages/Home/Error404.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Error404 = () => {
+
+  setTimeout(() => {
+    window.location.href = '/';
+  }, 2000);
+
   return (
     <div className='containerGlobalWeb'>
       <h1>404 - Página no encontrada</h1>
       <p>La página que estás buscando no existe.</p>
-      <Link to="/">Volver al inicio</Link>
+      {/* <Link to="/">Volver al inicio</Link> */}
     </div>
   );
 };
