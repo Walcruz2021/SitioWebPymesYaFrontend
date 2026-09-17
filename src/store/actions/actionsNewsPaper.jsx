c1599766c2daeb31ee77a456c2dccf588fe7ae1a
Walter Cruz <cruzherreria1@gmail.com>
2026-09-17 11:29:01 -0300

    handle error newsPapers

--- src/store/actions/actionsNewsPaper.jsx
+++ src/store/actions/actionsNewsPaper.jsx
@@ -7,11 +7,24 @@ export const GET_NEWS_PAPER_BY_ID = "GET_NEWS_PAPER_BY_ID";
 
 export const getNewsPaper = () => {
     return async (dispatch) => {
-        const response = await axios.get(`${rutaBackend}/api/newsPaper`);
+        try {
+            const response = await axios.get(`${rutaBackend}/api/newsPaper`);
 
-        dispatch({ type: GET_NEWS_PAPER, payload: response.data.listNews});
+            dispatch({
+                type: GET_NEWS_PAPER,
+                payload: response.data.listNews
+            });
+
+        } catch (error) {
+            console.error("Error al obtener las noticias:", error);
+
+            // dispatch({
+            //     type: GET_NEWS_PAPER_ERROR,
+            //     payload: error.message
+            // });
+        }
     };
-};  
+};
 
 export const addNewsPaper = (newsPaper) => {
     return async (dispatch) => {
